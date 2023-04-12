174fa9a5e6313d8e8be19e1ce44e9758f9166a70
Muhammad Hasbi Ar-Rasid <129012828+Ar-Rasid@users.noreply.github.com>
2023-04-12 20:04:02 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/a10120228latihan1/MainActivity.kt
+++ app/src/main/java/com/example/a10120228latihan1/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.a10120228latihan1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Tanggal : 12 April 2023
+// NIM     : 10120228
+// Nama    : Muhammad Hasbi Ar-Rasid
+// Kelas   : IF 6
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
